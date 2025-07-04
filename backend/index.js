675b4f347b0b73187db7f8e60edc5b930af16bc4
nowsadmunna <bsse1407@iit.du.ac.bd>
2025-07-04 22:19:20 +0600

    check deployment

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ app.post('/api/hallstudent',(req,res)=>{
   res.status(201).json('hall student added');
 });
 app.get('/',(req,res)=>{
-  res.send('Welcome to SHMMS API deployment');
+  res.send('Welcome to SHMMS API Deployment');
 });
 app.use('/api/student',studentRouter);
 app.use('/api/auth',authRouter);
